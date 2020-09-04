b6926b33fe84e3028f64ca8248e2df90a234d2c4
surya <techbizzsoftwaresolution@gmail.com>
2020-09-04 10:06:00 +0530

    first commit

---
+++ src/Overview.jsx
@@ -0,0 +1,364 @@
+import React from 'react';
+import fb from '../src/images/fb.png';
+import snap from '../src/images/snap.png';
+import insta from '../src/images/instagram.png';
+import tw from '../src/images/tw.png';
+import ChartsPage from './Dogh';
+import BarChart from './Bar';
+import add from '../src/images/tw.png';
+import fb1 from '../src/images/fb1.png';
+
+
+
+
+
+
+
+
+
+function Overview() {
+
+
+    return (
+        <>
+        <div className='left-side-menu'></div>
+        <div className='content-page' >
+       
+
+        <div className="container " id='overview-10'>
+
+
+        <div className="row">
+      
+        <div className='col-md-6 mt-5'>
+          <div className='recent-9'>
+        <h3 >Recent Notification</h3>
+        </div>
+    <div className='row'>
+        <div className='col-md-12 mt-2'>
+            <div className='row'>
+                <div className='col-md-7'>
+                
+                    
+                <h5 className="recent-title">Special title treatment</h5>
+               <p className="recent-text">With supporting text below as a natural lead-in to additional content.</p>
+              
+               </div>
+               
+                <div className='col-md-2 '>
+                <a href="#" className="btn btn-primary "><h6>Comment</h6> </a>
+                
+
+                </div>
+                <div className='col-md-3 mt-3 recent-hour'>
+                <h6>2 hours ago</h6>
+            
+                </div>
+                <div className='col-md-7'>
+                
+                    
+                <h5 className="recent-title">Special title treatment</h5>
+               <p className="recent-text">With supporting text below as a natural lead-in to additional content.</p>
+              
+               </div>
+               
+                <div className='col-md-2 '>
+                <a href="#" className="btn btn-primary "><h6>Comment</h6> </a>
+                
+
+                </div>
+                <div className='col-md-3 mt-3 recent-hour'>
+                <h6>2 hours ago</h6>
+            
+                </div>
+                <div className='col-md-7'>
+                
+                    
+                <h5 className="recent-title">Special title treatment</h5>
+               <p className="recent-text">With supporting text below as a natural lead-in to additional content.</p>
+              
+               </div>
+               
+                <div className='col-md-2 '>
+                <a href="#" className="btn btn-primary "><h6>Comment</h6> </a>
+                
+
+                </div>
+                <div className='col-md-3 mt-3 recent-hour'>
+                <h6>2 hours ago</h6>
+            
+                </div>
+                <div className='col-md-7'>
+                
+                    
+                <h5 className="recent-title">Special title treatment</h5>
+               <p className="recent-text">With supporting text below as a natural lead-in to additional content.</p>
+              
+               </div>
+               
+                <div className='col-md-2 '>
+                <a href="#" className="btn btn-primary "><h6>Comment</h6> </a>
+                
+
+                </div>
+                <div className='col-md-3 mt-3 recent-hour'>
+                <h6>2 hours ago</h6>
+            
+                </div>
+                
+                </div>
+                </div>
+                
+            </div>
+        </div>
+ 
+
+  <div className="col-md-6 mt-5">
+    <div className='recent-9'>
+  <h3>Social Overview</h3>
+  </div>
+  <div className="row ">
+    <div className="card  col-md-6">
+      <div className='social-10'>
+     <img src={snap}/>
+     </div>
+     <div className="row ">
+        <div className='col-sm-4 social-11'>
+        <h6>310,125 </h6>
+        <p>+10,03% </p>
+        <a href="#" class="link-social" role="button">Likes</a>
+        
+        </div>
+        <div className='col-sm-4 social-11'>
+        <h6>310,125 </h6>
+        <p>+10,03% </p>
+        <a href="#" class="link-social" role="button">Chek ins</a>
+       
+        </div>
+        <div className='col-sm-4 social-11'>
+        <h6>310,125 </h6>
+        <p>+10,03% </p>
+        <a href="#" class="link-social" role="button">Chek ins</a>
+       
+        </div>
+        </div>
+      </div>
+      <div className="card  col-md-6">
+      <div className='social-10'>
+     <img src={insta}/>
+     </div>
+     <div className="row ">
+        <div className='col-sm-4 social-11'>
+        <h6>310,125 </h6>
+        <p>+10,03% </p>
+        <a href="#" class="link-social" role="button">Likes</a>
+        
+        </div>
+        <div className='col-sm-4 social-11'>
+        <h6>310,125 </h6>
+        <p>+10,03% </p>
+        <a href="#" class="link-social" role="button">Chek ins</a>
+       
+        </div>
+        <div className='col-sm-4 social-11'>
+        <h6>310,125 </h6>
+        <p>+10,03% </p>
+        <a href="#" class="link-social" role="button">Chek ins</a>
+       
+        </div>
+        </div>
+      </div>
+      <div className="card  col-md-6">
+      <div className='social-10'>
+     <img src={fb}/>
+     </div>
+     <div className="row ">
+        <div className='col-sm-4 social-11'>
+        <h6>310,125 </h6>
+        <p>+10,03% </p>
+        <a href="#" class="link-social" role="button">Likes</a>
+        
+        </div>
+        <div className='col-sm-4 social-11'>
+        <h6>310,125 </h6>
+        <p>+10,03% </p>
+        <a href="#" class="link-social" role="button">Chek ins</a>
+       
+        </div>
+        <div className='col-sm-4 social-11'>
+        <h6>310,125 </h6>
+        <p>+10,03% </p>
+        <a href="#" class="link-social" role="button">Chek ins</a>
+       
+        </div>
+        </div>
+      </div>
+      <div className="card  col-md-6">
+      <div className='social-10'>
+     <img src={tw}/>
+     </div>
+     <div className="row ">
+        <div className='col-sm-4 social-11'>
+        <h6>310,125 </h6>
+        <p>+10,03% </p>
+        <a href="#" class="link-social" role="button">Likes</a>
+        
+        </div>
+        <div className='col-sm-4 social-11'>
+        <h6>310,125 </h6>
+        <p>+10,03% </p>
+        <a href="#" class="link-social" role="button">Chek ins</a>
+       
+        </div>
+        <div className='col-sm-4 social-11'>
+        <h6>310,125 </h6>
+        <p>+10,03% </p>
+        <a href="#" class="link-social" role="button">Chek ins</a>
+       
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+        <div class="row ">
+          <div class="col-md-9 ">
+            <div class="row recent-card">
+              <div className="col-sm-2 ml-1  ">
+                <div className="row ">
+                  <img src={add} alt="add" />
+
+                  <div className="ml-1 mt-3">
+                    <div className="row  ">
+                      <span>sdsds</span>
+                    </div>
+                    <div className="row">
+                      <span>sdsds</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-2 ml-1  ">
+                <div className="row ">
+                  <img src={add} alt="add" />
+
+                  <div className="ml-1 mt-3">
+                    <div className="row  ">
+                      <span>sdsds</span>
+                    </div>
+                    <div className="row">
+                      <span>sdsds</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-2 ml-1  ">
+                <div className="row ">
+                  <img src={add} alt="add" />
+
+                  <div className="ml-1 mt-3">
+                    <div className="row  ">
+                      <span>sdsds</span>
+                    </div>
+                    <div className="row">
+                      <span>sdsds</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-2 ml-1  ">
+                <div className="row ">
+                  <img src={add} alt="add" />
+
+                  <div className="ml-1 mt-3">
+                    <div className="row  ">
+                      <span>sdsds</span>
+                    </div>
+                    <div className="row">
+                      <span>sdsds</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-2 ml-1  ">
+                <div className="row ">
+                  <img src={add} alt="add" />
+
+                  <div className="ml-1 mt-3">
+                    <div className="row  ">
+                      <span>sdsds</span>
+                    </div>
+                    <div className="row">
+                      <span>sdsds</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+              
+            </div>
+          </div>
+
+          <div class="col-md-3 ">
+            <div className='row recent-card'>
+             <div className='col-md-12'>
+               <div className='row mt-4 pb-4 '>
+                 
+                   <div className='col-md-2'>
+                   <img src={fb1}/>
+                   </div>
+                   <div className='col-md-2'>
+                   <img src={fb1}/>
+                   </div>
+                   <div className='col-md-2'>
+                   <img src={fb1}/>
+                   </div>
+                   <div className='col-md-2'>
+                   <img src={fb1}/>
+                   </div>
+                   <div className='col-md-2'>
+                   <img src={fb1}/>
+                   </div>
+                   <div className='col-md-2'>
+                   vhyv
+                   </div>
+                   </div>
+          
+                 </div>
+                
+
+               
+            </div>
+          </div>
+        </div>
+     
+
+
+<div class="row mt-3">
+  <div class="col-md-4">
+    <div class="card4">
+  
+    <ChartsPage/>
+
+  </div>
+  </div>
+  <div class="col-md-8">
+    <div class="card4">
+     <BarChart/>
+    </div>
+  </div>
+</div>
+
+
+
+
+</div>
+        </div>
+        </>
+    )
+}
+
+export default Overview;
---
+++ src/Profile.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+
+
+function Profile() {
+    return (
+        <>
+        <div className='left-side-menu'></div>
+        <div className='content-page'  id='overview-10' >
+       
+
+        <div className="container mt-5 ">
+            <div className='setting-10'>
+                <h3>Profile Setting</h3>
+            </div>
+           <div className='row mt-4'>
+               <div className='col-md-3 setting-11'>
+                   <div className='row text-center '>
+                       <div className='col-md-12 account-10'>
+                      <h3> Account</h3>
+                       </div>
+                       <div className='col-md-12 account-11'>
+                       <h3>Notification Setting</h3>
+                       </div>
+                       <div className='col-md-12 account-11'>
+                      <h3> Biling</h3>
+                       </div>
+                       <div className='col-md-12 account-11'>
+                      <h3> Integrations</h3>
+                       </div>
+                       <div className='col-md-12 account-11'>
+                       <h3>Agency Setting</h3> 
+                       </div>
+                   </div>
+                  
+                   </div>
+                   <div className='col-md-8 ml-3 '>
+                      <div className='row'>
+                          <div className='col-md-3 setting-12 pt-3'>
+                           <h3>My Profile</h3>
+                          </div>
+                          <div className='col-md-3 setting-13 pt-3'>
+                          <h3>Company Profile</h3>
+                          </div>
+                          </div>
+                          <div className='row setting-14'>
+                              <div className='col-md-12 '>
+                                  <div className='row'>
+
+                            
+                              <div className='col-md-4 '>
+                              image cropper will display soon
+                              </div>
+                              <div className='col-md-8 '>
+                              <div class="form-group row mt-5">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Email:</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Email"/>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Password:</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Password"/>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Role:</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Admin"/>
+    </div>
+  </div>
+   <div className='pb-3 pt-3'> 
+  <button type="submit" class="btn btn-profile-10 my-1">Save</button>
+  </div>
+                              </div>
+                          </div>
+                      
+                   </div>
+  
+                   </div>
+                   </div>
+                   
+
+               
+               
+               </div>
+
+
+
+
+
+
+
+
+
+    </div>
+     </div>
+           
+</>
+    )
+}
+
+export default Profile
