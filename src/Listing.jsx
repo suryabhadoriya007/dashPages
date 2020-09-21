4a91e2d79e61e03b3964dad05dea343cb6787187
surya <techbizzsoftwaresolution@gmail.com>
2020-09-21 20:57:12 +0530

    thirdcommit

--- src/Listing.jsx
+++ src/Listing.jsx
@@ -1,200 +1,215 @@
-import React from 'react'
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import React from "react";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
 function Listing() {
-    return (
-        <>
-            <div className='left-side-menu'></div>
-        <div className='content-page'   >
-       
-
-        <div className='container'  id='overview-10'>
-            <div className='col-12 '>
-                <div className='list-head'>
-                Listing
+  return (
+    <>
+      <div className="left-side-menu"></div>
+      <div className="content-page">
+        <div className="container" id="overview-10">
+          <div className="col-12 ">
+            <div className="list-head">Listing</div>
+            <div className="card_list">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="row card-12 gap-10 textchange-10">
+                    <div className="col-md-3">Company name</div>
+                    <div className="col-md-3">Address</div>
+                    <div className="col-md-3"> Phone number</div>
+                    <div className="col-md-3"> Action</div>
+                  </div>
+                  <hr className="linecard"></hr>
+                  <div className="row card-12">
+                    <div className="col-md-3 textchange-10">
+                      <img 
+                        src={require("../src/images/Rectangle 326.png")}
+                        alt=""
+                      />
+                      El Dorado
+                    </div>
+                    <div className="col-md-3 textchange-10 res-list">
+                      <p>801 N Orange AVE rando, FL 32801</p>
+                    </div>
+                    <div className="col-md-3 res-list textchange-10">
+                      407-650-1811
+                    </div>
+                    <div className="col-md-3">
+                      <div className=" col-md-10 textchange-10 listingsbt-10 ">
+                        <div className="rest-list">View Listing </div>
+                      </div>
+                    </div>
+                  </div>
+                  <hr className="linecard"></hr>
+                  <div className="row card-12">
+                    <div className="col-md-3 textchange-10">
+                      <img
+                        src={require("../src/images/Rectangle 326.png")}
+                        alt=""
+                      />
+                      El Dorado
+                    </div>
+                    <div className="col-md-3 textchange-10 res-list">
+                      <p>801 N Orange AVE rando, FL 32801</p>
+                    </div>
+                    <div className="col-md-3 res-list textchange-10">
+                      407-650-1811
+                    </div>
+                    <div className="col-md-3">
+                      <div className=" col-md-10 textchange-10 listingsbt-10 ">
+                        <div className="rest-list">View Listing </div>
+                      </div>
+                    </div>
+                  </div>
+                  <hr className="linecard"></hr>
+                  <div className="row card-12">
+                    <div className="col-md-3 textchange-10">
+                      <img
+                        src={require("../src/images/Rectangle 326.png")}
+                        alt=""
+                      />
+                      El Dorado
+                    </div>
+                    <div className="col-md-3 textchange-10 res-list">
+                      <p>801 N Orange AVE rando, FL 32801</p>
+                    </div>
+                    <div className="col-md-3 res-list textchange-10">
+                      407-650-1811
+                    </div>
+                    <div className="col-md-3">
+                      <div className=" col-md-10 textchange-10 listingsbt-10 ">
+                        <div className="rest-list">View Listing </div>
+                      </div>
+                    </div>
+                  </div>
+                  <hr className="linecard"></hr>
+                  <div className="row card-12">
+                    <div className="col-md-3 textchange-10">
+                      <img
+                        src={require("../src/images/Rectangle 326.png")}
+                        alt=""
+                      />
+                      El Dorado
+                    </div>
+                    <div className="col-md-3 textchange-10 res-list">
+                      <p>801 N Orange AVE rando, FL 32801</p>
+                    </div>
+                    <div className="col-md-3 res-list textchange-10">
+                      407-650-1811
+                    </div>
+                    <div className="col-md-3">
+                      <div className=" col-md-10 textchange-10 listingsbt-10 ">
+                        <div className="rest-list">View Listing </div>
+                      </div>
+                    </div>
+                  </div>
+                  <hr className="linecard"></hr>
+                  <div className="row card-12">
+                    <div className="col-md-3 textchange-10">
+                      <img
+                        src={require("../src/images/Rectangle 326.png")}
+                        alt=""
+                      />
+                      El Dorado
+                    </div>
+                    <div className="col-md-3 textchange-10 res-list">
+                      <p>801 N Orange AVE rando, FL 32801</p>
+                    </div>
+                    <div className="col-md-3 res-list textchange-10">
+                      407-650-1811
+                    </div>
+                    <div className="col-md-3">
+                      <div className=" col-md-10 textchange-10 listingsbt-10 ">
+                        <div className="rest-list">View Listing </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className='card_list'>
-          <div className='row'>
-              <div className='col-md-12'>
-                  <div className='row card-12 gap-10 textchange-10'>
-                      <div className='col-md-3'>Company name</div>
-                      <div className='col-md-3'>Address</div>
-                      <div className='col-md-3'> Phone number</div>
-                      <div className='col-md-3'> Action</div>
-                  </div>
-                  <hr className='linecard'></hr>
-                  <div className='row card-12'>
-                      <div className='col-md-3 textchange-10'>
-                          
-                              
-                                <img src={require('../src/images/Rectangle 326.png')} alt=""/>
-                               El Dorado
-                             
-                              
-                          
-                      </div>
-                      <div className='col-md-3 textchange-10 res-list'>801 N Orange AVE rando, FL 32801</div>
-                      <div className='col-md-3 res-list textchange-10'>407-650-1811 </div>
-                      <div className='col-md-3'>
-                          <div className=' col-md-10 textchange-10 listingsbt-10 '>
-                              <div className='rest-list'>View Listing </div> 
-                          </div>
-                      </div>
-                  </div>
-                  <hr className='linecard'></hr>
-                  <div className='row card-12'>
-                      <div className='col-md-3 textchange-10'>
-                          
-                              
-                                <img src={require('../src/images/Rectangle 326.png')} alt=""/>
-                               El Dorado
-                             
-                              
-                          
-                      </div>
-                      <div className='col-md-3 textchange-10 res-list'>801 N Orange AVE rando, FL 32801</div>
-                      <div className='col-md-3 res-list textchange-10'>407-650-1811 </div>
-                      <div className='col-md-3'>
-                          <div className=' col-md-10 textchange-10 listingsbt-10 '>
-                              <div className='rest-list'>View Listing </div> 
-                          </div>
-                      </div>
-                  </div>
-                  <hr className='linecard'></hr>
-                  <div className='row card-12'>
-                      <div className='col-md-3 textchange-10'>
-                          
-                              
-                                <img src={require('../src/images/Rectangle 326.png')} alt=""/>
-                               El Dorado
-                             
-                              
-                          
-                      </div>
-                      <div className='col-md-3 textchange-10 res-list'>801 N Orange AVE rando, FL 32801</div>
-                      <div className='col-md-3 res-list textchange-10'>407-650-1811 </div>
-                      <div className='col-md-3'>
-                          <div className=' col-md-10 textchange-10 listingsbt-10 '>
-                              <div className='rest-list'>View Listing </div> 
-                          </div>
-                      </div>
-                  </div>
-                  <hr className='linecard'></hr>
-                  <div className='row card-12'>
-                      <div className='col-md-3 textchange-10'>
-                          
-                              
-                                <img src={require('../src/images/Rectangle 326.png')} alt=""/>
-                               El Dorado
-                             
-                              
-                          
-                      </div>
-                      <div className='col-md-3 textchange-10 res-list'>801 N Orange AVE rando, FL 32801</div>
-                      <div className='col-md-3 res-list textchange-10'>407-650-1811 </div>
-                      <div className='col-md-3'>
-                          <div className=' col-md-10 textchange-10 listingsbt-10 '>
-                              <div className='rest-list'>View Listing </div> 
-                          </div>
-                      </div>
-                  </div>
-                  <hr className='linecard'></hr>
-                  <div className='row card-12'>
-                      <div className='col-md-3 textchange-10'>
-                          
-                              
-                                <img src={require('../src/images/Rectangle 326.png')} alt=""/>
-                               El Dorado
-                             
-                              
-                          
-                      </div>
-                      <div className='col-md-3 textchange-10 res-list'>801 N Orange AVE rando, FL 32801</div>
-                      <div className='col-md-3 res-list textchange-10'>407-650-1811 </div>
-                      <div className='col-md-3'>
-                          <div className=' col-md-10 textchange-10 listingsbt-10 '>
-                              <div className='rest-list'>View Listing </div> 
-                          </div>
-                      </div>
-                  </div>
-
-
-
               </div>
-          </div>
-                
             </div>
-            
-           <div className='row card-12 page-align'>
-           <div className='col-md-2  '>
-               <div className='bt-page-10'>
-                 Next
-               </div>
-           </div>
-           <div className='col-md-5'>
-               <div className='row '>
-               <div className='col-md-1'>
-              <a class="" href="#">1</a>
-           </div>
-           <div className='col-md-1'>
-              <a class="" href="#">2</a>
-           </div>
-           <div className='col-md-1'>
-              <a class="" href="#">3</a>
-           </div>
-           <div className='col-md-1'>
-              <a class="" href="#">4</a>
-           </div>
-           <div className='col-md-1'>
-              <a class="" href="#">5</a>
-           </div>
-           <div className='col-md-1'>
-              <a class="" href="#">6</a>
-           </div>
-           <div className='col-md-1'>
-              <a class="" href="#">7</a>
-           </div>
-           <div className='col-md-1'>
-              <a class="" href="#">8</a>
-           </div>
-           <div className='col-md-1'>
-              <a class="" href="#">9</a>
-           </div>
-           <div className='col-md-1'>
-              <a class="" href="#">10</a>
-           </div>
-           <div className='col-md-1'>
-              <a class="" href="#">11</a>
-           </div>
-           <div className='col-md-1'>
-              <a class="" href="#">12</a>
-           </div>
-           
-           </div>
-           </div>
-           <div className='col-md-2'>
-               <div className='bt-page-10'>
-                 Next
-               </div>
-               </div>
-           <div className='col-md-3'>
-               <div className='iteams-page'>
-                   5 items/page
-                   <ArrowDropDownIcon/>
-               </div>
-           </div>
-           </div>
+
+            <div className="row card-12 page-align">
+              <div className="col-md-2  ">
+                <div className="bt-page-10">Next</div>
+              </div>
+              <div className="col-md-5">
+                <div className="row ">
+                  <div className="col-md-1">
+                    <a class="" href="#">
+                      1
+                    </a>
+                  </div>
+                  <div className="col-md-1">
+                    <a class="" href="#">
+                      2
+                    </a>
+                  </div>
+                  <div className="col-md-1">
+                    <a class="" href="#">
+                      3
+                    </a>
+                  </div>
+                  <div className="col-md-1">
+                    <a class="" href="#">
+                      4
+                    </a>
+                  </div>
+                  <div className="col-md-1">
+                    <a class="" href="#">
+                      5
+                    </a>
+                  </div>
+                  <div className="col-md-1">
+                    <a class="" href="#">
+                      6
+                    </a>
+                  </div>
+                  <div className="col-md-1">
+                    <a class="" href="#">
+                      7
+                    </a>
+                  </div>
+                  <div className="col-md-1">
+                    <a class="" href="#">
+                      8
+                    </a>
+                  </div>
+                  <div className="col-md-1">
+                    <a class="" href="#">
+                      9
+                    </a>
+                  </div>
+                  <div className="col-md-1">
+                    <a class="" href="#">
+                      10
+                    </a>
+                  </div>
+                  <div className="col-md-1">
+                    <a class="" href="#">
+                      11
+                    </a>
+                  </div>
+                  <div className="col-md-1">
+                    <a class="" href="#">
+                      12
+                    </a>
+                  </div>
                 </div>
-             
-
-
-
-
-
-            </div> 
+              </div>
+              <div className="col-md-2">
+                <div className="bt-page-10">Next</div>
+              </div>
+              <div className="col-md-3">
+                <div className="iteams-page">
+                  5 items/page
+                  <ArrowDropDownIcon />
+                </div>
+              </div>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Listing
+export default Listing;
