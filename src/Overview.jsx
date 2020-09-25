3539510f2b8658472e4cbc3461f8e1b1eb5c8a62
surya <techbizzsoftwaresolution@gmail.com>
2020-09-25 21:24:39 +0530

    overvlit

--- src/Overview.jsx
+++ src/Overview.jsx
@@ -20,6 +20,7 @@ function Overview() {
               <div className="recent-9">
                 <h3>Recent Notification</h3>
               </div>
+              <div className='card7'>
               <div className="row">
                 <div className="col-md-12 special_tit">
                   <div className="row">
@@ -90,131 +91,10 @@ function Overview() {
                   </div>
                 </div>
               </div>
+              </div>
             </div>
 
-            {/* <div className="col-md-6 recent_noti ">
-            <div className="recent-9">
-                <h3>Social Overview</h3>
-              </div>
-              <div className="row ">
-                <div className="card  col-md-6">
-                  <div className="social-10">
-                    <img src={snap} />
-                  </div>
-                  <div className="row ">
-                    <div className="col-sm-4 social-11">
-                      <h6>310,125 </h6>
-                      <p>+10,03% </p>
-                      <a href="#" class="link-social" role="button">
-                        Likes
-                      </a>
-                    </div>
-                    <div className="col-sm-4 social-11">
-                      <h6>310,125 </h6>
-                      <p>+10,03% </p>
-                      <a href="#" class="link-social" role="button">
-                        Chek ins
-                      </a>
-                    </div>
-                    <div className="col-sm-4 social-11">
-                      <h6>310,125 </h6>
-                      <p>+10,03% </p>
-                      <a href="#" class="link-social" role="button">
-                        Chek ins
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="card  col-md-6">
-                  <div className="social-10">
-                    <img src={insta} />
-                  </div>
-                  <div className="row ">
-                    <div className="col-sm-4 social-11">
-                      <h6>310,125 </h6>
-                      <p>+10,03% </p>
-                      <a href="#" class="link-social" role="button">
-                        Likes
-                      </a>
-                    </div>
-                    <div className="col-sm-4 social-11">
-                      <h6>310,125 </h6>
-                      <p>+10,03% </p>
-                      <a href="#" class="link-social" role="button">
-                        Chek ins
-                      </a>
-                    </div>
-                    <div className="col-sm-4 social-11">
-                      <h6>310,125 </h6>
-                      <p>+10,03% </p>
-                      <a href="#" class="link-social" role="button">
-                        Chek ins
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                </div>
-
-                <div className="row ">
-                <div className="card  col-md-6">
-                  <div className="social-10">
-                    <img src={fb} />
-                  </div>
-                  <div className="row ">
-                    <div className="col-sm-4 social-11">
-                      <h6>310,125 </h6>
-                      <p>+10,03% </p>
-                      <a href="#" class="link-social" role="button">
-                        Likes
-                      </a>
-                    </div>
-                    <div className="col-sm-4 social-11">
-                      <h6>310,125 </h6>
-                      <p>+10,03% </p>
-                      <a href="#" class="link-social" role="button">
-                        Chek ins
-                      </a>
-                    </div>
-                    <div className="col-sm-4 social-11">
-                      <h6>310,125 </h6>
-                      <p>+10,03% </p>
-                      <a href="#" class="link-social" role="button">
-                        Chek ins
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="card  col-md-6">
-                  <div className="social-10">
-                    <img src={tw} />
-                  </div>
-                  <div className="row">
-                    <div className="col-sm-4 social-11">
-                      <h6>310,125 </h6>
-                      <p>+10,03% </p>
-                      <a href="#" class="link-social" role="button">
-                        Likes
-                      </a>
-                    </div>
-                    <div className="col-sm-4 social-11">
-                      <h6>310,125 </h6>
-                      <p>+10,03% </p>
-                      <a href="#" class="link-social" role="button">
-                        Chek ins
-                      </a>
-                    </div>
-                    <div className="col-sm-4 social-11">
-                      <h6>310,125 </h6>
-                      <p>+10,03% </p>
-                      <a href="#" class="link-social" role="button">
-                        Chek ins
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                </div>
-             
-            </div> */}
+           
            
           <div className='col-md-6  recent_noti'>
           <div className="recent-9">
@@ -356,107 +236,7 @@ function Overview() {
           </div>
           </div>
 
-          {/* <div class="row hawaipatti ">
-            <div class="col-md-8 ">
-              <div class="row recent-card">
-                <div className="col-sm-2 icon_margin  ">
-                  <div className="row ">
-                    <img src={add} alt="add" />
-
-                    <div className="icon_margin ">
-                      <div className="row  ">
-                        <span>sdsds</span>
-                      </div>
-                      <div className="row">
-                        <span>sdsds</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-sm-2 icon_margin  ">
-                  <div className="row ">
-                    <img src={add} alt="add" />
-
-                    <div className="icon_margin ">
-                      <div className="row  ">
-                        <span>sdsds</span>
-                      </div>
-                      <div className="row">
-                        <span>sdsds</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-sm-2 icon_margin  ">
-                  <div className="row ">
-                    <img src={add} alt="add" />
-
-                    <div className="icon_margin ">
-                      <div className="row  ">
-                        <span>sdsds</span>
-                      </div>
-                      <div className="row">
-                        <span>sdsds</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-sm-2 icon_margin  ">
-                  <div className="row ">
-                    <img src={add} alt="add" />
-
-                    <div className="icon_margin ">
-                      <div className="row  ">
-                        <span>sdsds</span>
-                      </div>
-                      <div className="row">
-                        <span>sdsds</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-sm-2 icon_margin  ">
-                  <div className="row ">
-                    <img src={add} alt="add" />
-
-                    <div className="icon_margin ">
-                      <div className="row  ">
-                        <span>sdsds</span>
-                      </div>
-                      <div className="row">
-                        <span>sdsds</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-4 ">
-              <div className="row recent-card">
-                <div className="col-md-12">
-                  <div className="row right_icons">
-                    <div className="col-md-2">
-                      <img src={fb1} />
-                    </div>
-                    <div className="col-md-2">
-                      <img src={fb1} />
-                    </div>
-                    <div className="col-md-2">
-                      <img src={fb1} />
-                    </div>
-                    <div className="col-md-2">
-                      <img src={fb1} />
-                    </div>
-                    <div className="col-md-2">
-                      <img src={fb1} />
-                    </div>
-                    <div className="col-md-2">vhyv</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> */}
+        
 
 
              <div className="row hawaipatti ">
